fd133f398306f01c795c988b9365d3021e879445
yKyubi <75593445+yKyubi@users.noreply.github.com>
2021-02-14 03:56:32 -0300

    Create suporte.java

---
+++ Komputer/suporte.java
@@ -0,0 +1,5 @@
+Pc = komputer * (Izuk4Bot2.0 + Izuk4Bot - Izuk4)
+           + 4 * bot; // PREFER
+
+Android = /0/android/suporte.java * (Izuk4Bot2.0 + Izuk4
+                       - Izuk4Bot) + 4 * bot; // AVOID
