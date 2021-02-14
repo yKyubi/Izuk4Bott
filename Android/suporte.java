0eccd0dd8eb63c7bda223bf9c320b9e44b48d675
yKyubi <75593445+yKyubi@users.noreply.github.com>
2021-02-14 03:52:11 -0300

    Create suporte.java

---
+++ Android/suporte.java
@@ -0,0 +1,9 @@
+/*
+ * Classname             (Izuk4Bot)
+ *
+ * Version information   (2.0)
+ *
+ * Date                  (06/12,15:35)
+ *
+ * author                (𝑴𝑰𝑫𝑶𝑹𝑰𝒀𝑨𝑲𝑲𝑲𝑲𝑲𝑲𝑲)
+ * Copyright notice      (bot com suporte a Android/Java)
